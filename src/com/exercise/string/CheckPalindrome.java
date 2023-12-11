26f17a697327cca31c462fda622991b08e869742
Deepan <62413325+Deepan-ks@users.noreply.github.com>
2023-12-11 20:07:05 +0530

    string added

--- src/com/exercise/string/CheckPalindrome.java
+++ src/com/exercise/string/CheckPalindrome.java
@@ -1,2 +1,27 @@
-package com.exercise.string;public class CheckPalindrome {
+package com.exercise.string;
+
+public class CheckPalindrome {
+    public static void main(String[] args) {
+        String str = "a ";
+        boolean res = palindrome(str);
+        if(res){
+            System.out.println("string is palindrome");
+        }else{
+            System.out.println("string is not palindrome");
+        }
+    }
+    static boolean palindrome(String str){
+
+        int start = 0;
+        int end = str.length() - 1;
+        while(start <= end){
+            if(str.charAt(start) == str.charAt(end)){
+                start++;
+                end--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/com/learn/StringBuilderTest.java
+++ src/com/learn/StringBuilderTest.java
@@ -1,2 +1,20 @@
-package com.learn;public class StringBuilderTest {
+package com.learn;
+
+public class StringBuilderTest {
+    public static void main(String[] args) {
+        String str = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i =0; i< 26; i++){
+            char ch = (char) ('a' + i);
+            str += ch;
+            //  loop 1 : 'a' + i -> "a" ; loop 2 : "a" + i - > "a" + 'b' -> "ab"
+            // "a", "ab", "abc", "abcd" ..... "a..z" -> creating new object and pointing to new object at next loop;
+            // after "ab" created, "a" was not pointed by reference/object; leads to usage of more memory space;
+            sb.append(ch);
+            // StringBuilder don't create new object, instead do change the original object;
+            // mutable like the arrays
+        }
+        System.out.println("String : "+ str);
+        System.out.println("StringBuilder : "+ sb);
+    }
 }
